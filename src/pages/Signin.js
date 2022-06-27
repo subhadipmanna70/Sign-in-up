e9708ab33c770e5beaa87dfd8567d9b425c60526
subhadipmanna70 <subhadipmanna70@gmail.com>
2022-06-27 18:42:43 +0530

    first try

---
+++ src/pages/Signin.js
@@ -0,0 +1,17 @@
+ 
+export default function Signin() {
+    return(
+        <div className='bg-gradient-to-t from-blue-800 to-white-300 h-[100vw] w-[100vw] flex items-center justify-center'>
+       <div className='bg-white/20 backdrop-blur-lg hover:backdrop-blur-in h-[450px] w-[300px] rounded-xl shadow-md font-light'>
+<div className='text-center text-3xl mt-10'>Sign in</div>
+
+
+
+
+
+       </div>
+        </div> 
+    )
+
+  
+}
